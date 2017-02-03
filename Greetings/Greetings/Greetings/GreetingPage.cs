8c76c2c582e17cfb458223983b5f6ad5bfd8a77c
daryldcL <daryl.lomugdang@gmail.com>
2017-02-03 17:28:43 +0800

    Practice Day 2

---
+++ Greetings/Greetings/Greetings/GreetingPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingPage : ContentPage
+    {
+        public GreetingPage()
+        {
+            this.Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms!",
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                //HorizontalOptions = LayoutOptions.Center,
+                //VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.Yellow,
+                TextColor = Color.Blue
+            };
+
+            //Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+            Device.OnPlatform(iOS: () =>
+            {
+                Padding = new Thickness(0, 20, 0, 0);
+            });
+        }
+    }
+}
--- HelloXamarin/HelloXamarin/HelloXamarin.Droid/MainActivity.cs
+++ HelloXamarin/HelloXamarin/HelloXamarin.Droid/MainActivity.cs
@@ -9,7 +9,8 @@ using Android.OS;
 
 namespace HelloXamarin.Droid
 {
-    [Activity(Label = "HelloXamarin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HelloXamarin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
