9ee08dd30c0909a0bb3804a664511fa55cfdd30a
daryldcL <daryl.lomugdang@gmail.com>
2017-02-14 09:09:53 +0800

    Embedded Resource - blackcat.txt

--- Greetings/Greetings/Greetings/GreetingPage.cs
+++ Greetings/Greetings/Greetings/GreetingPage.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Xamarin.Forms;
 
@@ -8,24 +11,31 @@ namespace Greetings
     {
         public GreetingPage()
         {
-            StackLayout stackLayout = new StackLayout
-            {
-                BackgroundColor = Color.Blue,
-                HorizontalOptions = LayoutOptions.Start,
-                Orientation = StackOrientation.Horizontal
-            };            
+            StackLayout stackLayout = new StackLayout();
 
-            foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
+            foreach(FieldInfo info in typeof(Color).GetRuntimeFields())
             {
                 if (info.GetCustomAttribute<ObsoleteAttribute>() != null)
                     continue;
 
-                if(info.IsPublic &&
+                if(info.IsPublic && 
                     info.IsStatic &&
                     info.FieldType == typeof(Color))
                 {
+                    stackLayout.Children.Add(CreateColorView((Color)info.GetValue(null), info.Name));
+                }
+            }
+
+            foreach(PropertyInfo info in typeof(Color).GetRuntimeProperties())
+            {
+                MethodInfo methodInfo = info.GetMethod;
+
+                if(methodInfo.IsPublic &&
+                    methodInfo.IsStatic &&
+                    methodInfo.ReturnType == typeof(Color))
+                {
                     stackLayout.Children.Add(
-                        CreateColorLabel((Color)info.GetValue(null), info.Name));
+                        CreateColorView((Color)info.GetValue(null), info.Name));
                 }
             }
 
@@ -33,31 +43,61 @@ namespace Greetings
 
             Content = new ScrollView
             {
-                Content = stackLayout,
-                Orientation = ScrollOrientation.Horizontal,
-                BackgroundColor = Color.Red
+                Content = stackLayout
             };
         }
 
-        private Label CreateColorLabel(Color color, string name)
+        private View CreateColorView(Color color, string name)
         {
-            Color background = Color.Default;
-
-            if (color != Color.Default)
+            return new Frame
             {
-                double luminance = 0.30 * color.R +
-                                   0.59 * color.G +
-                                   0.11 * color.B;
-
-                background = luminance > 0.5 ? Color.Black : Color.White;
-            }
-
-            return new Label
-            {
-                Text = name,
-                TextColor = color,
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                BackgroundColor = background
+                OutlineColor = Color.Accent,
+                Padding = new Thickness(5),
+                Content = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    Spacing = 15,
+                    Children =
+                    {
+                        new BoxView
+                        {
+                            Color = color
+                        },
+                        new Label
+                        {
+                            Text = name,
+                            FontSize = Device.GetNamedSize(NamedSize.Large,typeof(Label)),
+                            FontAttributes = FontAttributes.Bold,
+                            VerticalOptions = LayoutOptions.Center,
+                            HorizontalOptions = LayoutOptions.StartAndExpand
+                        },
+                        new StackLayout
+                        {
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = String.Format("{0:X2}-{1:X2}-{2:X2}",
+                                                          (int)(255 * color.R),
+                                                          (int)(255 * color.G),
+                                                          (int)(255 * color.B)),
+                                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                                    IsVisible = color != Color.Default
+                                },
+                                new Label
+                                {
+                                    Text = String.Format("{0:F2}-{1:F2}-{2:F2}",
+                                                          color.Hue,
+                                                          color.Saturation,
+                                                          color.Luminosity),
+                                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                                    IsVisible = color != Color.Default
+                                }
+                            },
+                            HorizontalOptions = LayoutOptions.End
+                        }
+                    }
+                }
             };
         }
     }
